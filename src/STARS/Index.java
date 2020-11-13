21882971adf222f11f82425daf2e91c526c062f3
luciuslee98 <74395381+luciuslee98@users.noreply.github.com>
2020-11-13 15:37:45 +0800

    Create Index.java

---
+++ src/STARS/Index.java
@@ -0,0 +1,45 @@
+package stars;
+
+
+public class Index {
+	private String Name;
+	private int Vacancy;
+	private List<String> StudentsRegistered;
+	
+	public Index(String name,int vacancy) {
+		Name=name;
+		Vacancy=vacancy;
+		StudentsRegistered= new ArrayList<String>();
+	}
+	
+	public void PrintStudentList() {
+		for (int i=0;i<StudentsRegistered.size();i++) {
+			System.out.println(StudentsRegistered[i]);
+		}
+	}
+	
+	public Boolean CheckVacancy() {
+		if (Vacancy>0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void SetIndex(String newindex) {
+		Name = newindex;
+	}
+	
+	public String GetIndex() {
+		return Name;
+	}
+	
+	public void SetVacancy(int newvacancy) {
+		Vacancy=newvacancy;
+	}
+	
+	public int GetVacancy() {
+		return Vacancy;
+	}
+}
